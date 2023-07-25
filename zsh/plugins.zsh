cb5877383fa555580d76a3becff38c2f58f1f9a6
John DeSilva <desilvjo@umich.edu>
2023-07-25 18:11:29 -0400

    Install zsh-autosuggestions

--- zsh/plugins.zsh
+++ zsh/plugins.zsh
@@ -3,6 +3,8 @@ source $ZPLUG_HOME/init.zsh
 
 zplug "plugins/git", from:oh-my-zsh
 
+zplug "zsh-users/zsh-autosuggestions"
+
 # Install plugins if there are plugins that have not been installed
 if ! zplug check --verbose; then
     printf "Install? [y/N]: "
